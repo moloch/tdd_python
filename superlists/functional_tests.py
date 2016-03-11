0f8af434ccb4de3a70bfc15f7a88af3bd5220600
XPeppers <setefi@xpeppers.com>
2016-03-11 14:50:54 +0100

    fist post implementation

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.keys import Keys
+import unittest, time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -17,6 +18,21 @@ class NewVisitorTest(unittest.TestCase):
     header_text = self.browser.find_element_by_tag_name('h1').text
     self.assertIn('To-Do', header_text)
 
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(
+            'Enter a to-do item',
+            inputbox.get_attribute('placeholder')
+    )
+
+    inputbox.send_keys('Buy peacock feathers')
+    inputbox.send_keys(Keys.ENTER)
+
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertTrue(
+        any(row.text == '1: Buy peacock feathers' for row in rows),
+        'New to-do item did not appear in table'
+    )
 
 if __name__ == '__main__':
   unittest.main()
